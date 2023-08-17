aa58dfeae922126e98b0019a25c954b93a0cb77e
miguel1man <miguelangelhh17@gmail.com>
2023-08-16 21:29:48 -0500

    update chat style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
 	let isShowSourcesMode = false
   let responseData: ResponseData | null = null
 
-  function toggleMode() {
+  function toggleSourceMode() {
     isShowSourcesMode = !isShowSourcesMode
   }
 
@@ -23,7 +23,6 @@
 		
 		if (isShowSourcesMode) {
 			responseData = await apiVectorDB(currentMessage, "id_1")
-			console.log("response data:", responseData)
 		}
 
 		const updatedChunks = await handleChatQuestion(currentMessage, chunks)
@@ -37,54 +36,52 @@
 	}
 </script>
 
-<div class="container h-full mx-auto flex flex-col justify-center items-center space-y-20">
-	<div class="min-w-[50em] max-w-lg mx-auto">
-		<div class="flex flex-col justify-end space-y-2">
-			{#each messages as message (message.content)}
-        <div class="flex justify-{message.isQuestion ? 'end' : 'start'} space-x-2">
-          <div class="card p-4 rounded-{message.isQuestion ? 'tr' : 'tl'}-none space-y-2 {message.isQuestion ? 'variant-primary' : 'variant-soft-secondary'}">
-            <p>{message.content}</p>
-          </div>
-        </div>
-      {/each}
+<div class="my-2 mx-auto flex flex-col justify-center items-center space-y-20">
+	<div class="max-w-screen-lg md:max-w-screen-xl xl:max-w-[100em] mx-auto grid gap-4 lg:grid-cols-2">
+		<div class="card rounded-4 xl:w-[40em] bg-tertiary-500 bg-opacity-10 space-y-2 p-2">
+			<div class="h-[calc(100vh-10em)] flex flex-col space-y-2 overflow-y-auto">
+				{#each messages as message (message.content)}
+					<div class="card p-4 max-w-[95%] {message.isQuestion ? 'variant-primary rounded-tr-none ml-auto justify-end' : 'variant-soft-secondary rounded-tl-none mr-auto justify-start'}">
+						<p>{message.content}</p>
+					</div>
+				{/each}
+			</div>
+
+			<div class="input-group input-group-divider grid-cols-[auto_1fr_auto] rounded-container-token gap-4">
+				<button class="input-group-shim">+</button>
+				<textarea
+					bind:value={currentMessage}
+					class="bg-transparent border-0 ring-0"
+					name="prompt"
+					id="prompt"
+					placeholder="Write a message..."
+					rows="1"
+				/>
+				<button class="variant-filled-primary" on:click={sendMessage}>
+					Send
+				</button>
+			</div>
 		</div>
 
-		<div class="input-group input-group-divider grid-cols-[auto_1fr_auto] rounded-container-token my-3">
-			<button class="input-group-shim">+</button>
-			<textarea
-				bind:value={currentMessage}
-				class="bg-transparent border-0 ring-0"
-				name="prompt"
-				id="prompt"
-				placeholder="Write a message..."
-				rows="1"
-			/>
-			<button class="variant-filled-primary" on:click={sendMessage}>
-				Send
-			</button>
-		</div>
-
-		<div class="flex justify-center">
-			<SlideToggle name="slider-label" checked={isShowSourcesMode} active="bg-primary-500" on:click={toggleMode}>
+		<div class="card rounded-4 bg-tertiary-500 bg-opacity-10 space-y-2 p-2 w-full mt-4 lg:mt-0">
+			<SlideToggle name="slider-sources" checked={isShowSourcesMode} active="bg-primary-500" on:click={toggleSourceMode}>
 				{#if isShowSourcesMode}
 					Show Sources
 				{:else}
 					Quick Chat
 				{/if}
 			</SlideToggle>
+			{#if responseData}
+				<div>
+					<h2>Response Data:</h2>
+					{#each responseData.content as contentItem (contentItem.metadata.file)}
+						<p>File: {contentItem.metadata.file}</p>
+						<p>Header: {contentItem.metadata.header}</p>
+						<p>Score: {contentItem.score}</p>
+						<p>Text: {contentItem.text}</p>
+					{/each}
+				</div>
+			{/if}
 		</div>
-
-		{#if responseData}
-			<div>
-				<h2>Response Data:</h2>
-				{#each responseData.content as contentItem (contentItem.metadata.file)}
-					<p>File: {contentItem.metadata.file}</p>
-					<p>Header: {contentItem.metadata.header}</p>
-					<p>Score: {contentItem.score}</p>
-					<p>Text: {contentItem.text}</p>
-				{/each}
-			</div>
-		{/if}
-
 	</div>
 </div>
